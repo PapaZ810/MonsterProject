fcf75c26a90a68b00f85b8272c51ae19c50c4bc6
Zac <zombiezac@att.net>
2018-09-11 12:13:09 -0600

    added info to controller and model packages

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -1,16 +1,21 @@
 package controller;
 
+import model.MarshmallowMonster;
+
 public class MonsterController
 {
+	//Data member section or declaration section
+	private MarshmallowMonster myMonster;
+	
+	//Constructors initialize data members
+	public MonsterController()
+	{
+		myMonster = new MarshmallowMonster();
+	}
+	
 	public void start()
 	{
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(myMonster);
 		
 	}
 }
---
+++ src/model/MarshmallowMonster.java
@@ -0,0 +1,17 @@
+package model;
+
+public class MarshmallowMonster
+{
+	public MarshmallowMonster()
+	{
+		//Default constructor 
+		//boring and not as helpful
+	}
+	public String toString()
+	{
+		String description = "";
+		
+		return description;
+	}
+	
+}
