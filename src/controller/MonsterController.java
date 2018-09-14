ece800dd810d29910b2211c1bf4331e6e611a93e
Zac <zombiezac@att.net>
2018-09-14 12:56:44 -0600

    Gui elements added and old sysout elements commented out

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.MarshmallowMonster;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class MonsterController
 {
@@ -20,36 +21,100 @@ public class MonsterController
 	
 	public void start()
 	{
+		//JOptionPane.showMessageDialog(null,myMonster);
+		
 		System.out.println(myMonster);
 		
-		System.out.println("How many legs do you want your monster to have?");
-		double legCount = inputScanner.nextDouble();
+		//System.out.println("How many legs do you want your monster to have?");
+		//JOptionPane.showMessageDialog(null, "How many \nlegs do you want your monster to have?");
+		String userInput = JOptionPane.showInputDialog(null, "How many legs do you want yourmonster to have?");
+		double legCount = -99;
+		if (validDouble(userInput))
+		{
+			legCount = Double.parseDouble(userInput);
+		}
+		//double legCount = inputScanner.nextDouble();
 		userMonster.setLegCount(legCount);
 		
-		System.out.println("How many arms do you want your monster to have?");
-		int armCount = inputScanner.nextInt();
+		//System.out.println("How many arms do you want your monster to have?");
+		//JOptionPane.showMessageDialog(null, "How many arms do you want your monster to have?");
+		userInput = JOptionPane.showInputDialog(null, "How many arms do you want your monster to have?");
+		int armCount = -99;
+		if (validInt(userInput))
+		{
+			armCount = Integer.parseInt(userInput);
+		}
+		//int armCount = inputScanner.nextInt();
 		userMonster.setArmCount(armCount);
 		
-		System.out.println("Do you want your monster to have a nose? True or false only please!");
-		boolean hasNoses = inputScanner.nextBoolean();
-		userMonster.setHasNoses(hasNoses);
+		//System.out.println("Do you want your monster to have a nose? True or false only please!");
+		//JOptionPane.showMessageDialog(null, "Do you want you monster to have a nose?");
+		userInput = JOptionPane.showInputDialog(null, "Do you want your monster to have a nose?");
+		boolean answer = Boolean.parseBoolean(userInput);
+		//boolean hasNoses = inputScanner.nextBoolean();
+		userMonster.setHasNoses(answer);
 		
-		System.out.println("How many eyes do you want your monster to have?");
-		int eyeCount = inputScanner.nextInt();
+		//System.out.println("How many eyes do you want your monster to have?");
+		userInput = JOptionPane.showInputDialog(null, "How many eyes do you want your monster to have?");
+		int eyeCount = -99;
+		if (validInt(userInput));
+		{
+			eyeCount = Integer.parseInt(userInput);
+		}
+		//int eyeCount = inputScanner.nextInt();
 		userMonster.setEyeCount(eyeCount);
 		
-		inputScanner.nextLine();
+		//inputScanner.nextLine();
 		
-		System.out.println("What is your monster's name?");
-		String name = inputScanner.nextLine();
-		userMonster.setName(name);
+		//System.out.println("What is your monster's name?");
+		//JOptionPane.showMessageDialog(null, "What is your monster's name?");
+		userInput = JOptionPane.showInputDialog(null, "What is you monster's name?");
+		//String name = inputScanner.nextLine();
+		userMonster.setName(userInput);
 		
-		System.out.println("What do you want your monster to say?");
-		String said = inputScanner.nextLine();
-		userMonster.setSaid(said);
+		//System.out.println("What do you want your monster to say?");
+		userInput = JOptionPane.showInputDialog(null, "What do you want your monster to say?");
+		//String said = inputScanner.nextLine();
+		userMonster.setSaid(userInput);
 		
-		System.out.println("Your monster has " + userMonster.getLegCount() + " legs and " + userMonster.getArmCount() +  " arms! Your monster has a " + userMonster.getHasNoses() + " nose and " + userMonster.getEyeCount() + " eyes! Your monster's name is " + userMonster.getName() + " and he says " + userMonster.getSaid() + "! Isn't that just so precious!");
+		//System.out.println("Your monster has " + userMonster.getLegCount() + " legs and " + userMonster.getArmCount() +  " arms! Your monster has a " + userMonster.getHasNoses() + " nose and " + userMonster.getEyeCount() + " eyes! Your monster's name is " + userMonster.getName() + " and he says " + userMonster.getSaid() + "! Isn't that just so precious!");
+		
+		JOptionPane.showMessageDialog(null, "Your monster has " + userMonster.getLegCount() + " legs and " + userMonster.getArmCount() +  " arms! \nYour monster has a " + userMonster.getHasNoses() + " nose and " + userMonster.getEyeCount() + " eyes! \nYour monster's name is " + userMonster.getName() + " and he says " + userMonster.getSaid() + "! Isn't that just so precious!");
 		
 		inputScanner.close();
 	}
+		
+			public boolean validInt(String maybeInt)
+			{
+				boolean isValid = false;
+				
+				try
+				{
+					Integer.parseInt(maybeInt);
+					isValid = true;
+				}
+				catch (NumberFormatException error)
+				{
+					JOptionPane.showMessageDialog(null, "You need to type in a whole number!");
+				}
+				return true;
+			}
+			
+			public boolean validDouble(String mightBeDouble)
+			{
+				boolean isValid = false;
+				
+				try
+				{
+					Double.parseDouble(mightBeDouble);
+					isValid = true;
+				}
+				catch (NumberFormatException error)
+				{
+					JOptionPane.showMessageDialog(null,	"You need to type in a rational number");
+				}
+				return true;
+			}
+			
+			
 }
